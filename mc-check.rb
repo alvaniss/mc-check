855e199dc050d365c6f3b9ebf23d3282ba4fec82
Alvaniss <oneweek1001@gmail.com>
2025-01-20 13:06:53 +0600

    Fix installation files for homebrew

--- mc-check.rb
+++ mc-check.rb
@@ -10,8 +10,14 @@ class McCheck < Formula
   depends_on "python@3.9"
 
   def install
-    virtualenv_install_with_resources
-  end
+    venv = virtualenv_create(libexec, "python3")
+    venv.pip_install "requests"
+    venv.pip_install "urllib3"
+    venv.pip_install "idna"
+    venv.pip_install "certifi"
+    venv.pip_install "chardet"
+    venv.pip_install_and_link buildpath
+  end  
 
   test do
     assert_match "Usage", shell_output("#{bin}/mc_check --help")
