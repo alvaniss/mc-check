5b5f4a87589ae2246986dd8d0de57165899c6dec
Alvaniss <oneweek1001@gmail.com>
2025-01-20 13:17:52 +0600

    Update sha256

--- mc-check.rb
+++ mc-check.rb
@@ -4,7 +4,7 @@ class McCheck < Formula
   desc "CLI tool to check Minecraft server status using the mcsrvstat.us API"
   homepage "https://github.com/alvaniss/mc-check"
   url "https://github.com/alvaniss/mc-check/archive/refs/tags/v1.0.2.tar.gz"
-  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  sha256 "8a12827656e57ca96abd4182de80ae313c4c22197b8e31d2c669e34ffc380696"
   license "GPL-3.0"
 
   depends_on "python@3.9"
